0d76edfc225ce4c17a0c01d130a1845fe539f84a
Nathan Journo <nathanjourno@yahoo.fr>
2020-04-17 14:41:12 +0200

    externalisation program.cs

--- Pokemon/Program.cs
+++ Pokemon/Program.cs
@@ -2,6 +2,7 @@
 
 namespace PokemonGame
 {
+
     class MainClass
     {
 
@@ -9,19 +10,37 @@ namespace PokemonGame
         //static string[] faiblesses_eau = new string[] { "Plante", "Ultime" };
         //static string[] faiblesses_plante = new string[] { "Feu", "Ultime" };
 
+        static string result;
+        static int i_result;
+        static Game pokemonGame;
+        static Starter starter;
+        static Character player;
         public static void Main(string[] args)
         {
-            string result;
-            int i_result;
-            Game pokemonGame;
-            Starter starter;
-            newGame:
 
+            bool keepGoing = true;
+
+            do
+            {
+                difficultyChoice();
+
+                nameChoice();
+
+                pokemonChoice();
+
+                keepGoing = rebootGame();
+
+            } while (keepGoing);
+
+
+        }
+        private static void difficultyChoice()
+        {
             Console.WriteLine("Bienvenue dans Pokemon Fight!");
             Console.WriteLine("Gagne les 5 combats pour terminer la ligue Pokemon et deviens maître Pokemon!");
             Console.WriteLine("Choisis ta difficulté: 'tranquille' ou 'chaud' ");
             result = Console.ReadLine();
-            if (result == "tranquille" || result =="chaud")
+            if (result == "tranquille" || result == "chaud")
             {
                 pokemonGame = new Game(result);
             }
@@ -29,12 +48,18 @@ namespace PokemonGame
             {
                 pokemonGame = new Game("tranquille");
             }
-            Console.WriteLine("* Prof Chen parle *");
+        }
+        private static void nameChoice()
+        {
             Console.WriteLine("Bien le bonjour! Quel est ton nom?");
             result = Console.ReadLine();
-            Character player = new Character(result);
+            player = new Character(result);
             Console.ForegroundColor = ConsoleColor.Magenta;
             Console.WriteLine("Bienvenue " + player.Name + " dans le monde magique des Pokemon! Mon nom est Chen! Les gens souvent m'appellent le Prof Pokemon! Ce monde est peuple de creatures du nom de Pokemon! L'etude des Pokemon est ma profession. Ta quête pour devenir maître Pokemon est sur le point de commencer! Un tout nouveau monde de rêves, d'aventures et de Pokemon t'attend! Dingue!");
+
+        }
+        private static void pokemonChoice()
+        {
             Console.WriteLine("A present, choisis parmi ces 3 Pokemons! Prends-en bien soin.");
             Console.WriteLine("Veux-tu le Pokemon type Plante: Bulbizarre? Tape 1");
             Console.WriteLine("Veux-tu le Pokemon type Feu: Salameche? Tape 2");
@@ -42,29 +67,29 @@ namespace PokemonGame
             Console.WriteLine("Tu ne sais pas qui tu veux? Tape 4 pour en obtenir un aleatoirement");
             i_result = Int16.Parse(Console.ReadLine());
             Random rnd = new Random();
-            retry:
+        retry:
             switch (i_result)
             {
                 case 1:
-                    
+
                     Console.WriteLine("* vous avez obtenu Bulbizarre *");
-                   
+
                     starter = new Bulbizarre();
                     pokemonGame.generateFights(i_result, starter);
                     break;
 
                 case 2:
-                    
+
                     Console.WriteLine("* vous avez obtenu Salameche *");
-                   
+
                     starter = new Salamèche();
                     pokemonGame.generateFights(i_result, starter);
                     break;
 
                 case 3:
-                    
+
                     Console.WriteLine("* vous avez obtenu Carapuce  *");
-                    
+
                     starter = new Carapuce();
                     pokemonGame.generateFights(i_result, starter);
                     break;
@@ -77,20 +102,24 @@ namespace PokemonGame
                 default:
                     break;
             }
+        }
+        private static bool rebootGame()
+        {
             Console.WriteLine("*****************************");
             Console.WriteLine("Voulez-vous recommencer une partie? oui/non");
 
             result = Console.ReadLine();
 
-            if ( result == "oui" )
+            if (result != "oui")
             {
-                goto newGame;
+                Console.WriteLine("A la prochaine " + player.Name + " !");
+
+                return false;
             }
             else
             {
-                Console.WriteLine("A la prochaine " + player.Name + " !");
+                return true;
             }
-
         }
     }
 }
